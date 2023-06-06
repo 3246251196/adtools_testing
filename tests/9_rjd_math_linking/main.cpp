f3f10e6702a54a8c772bea57d228264521f8e1c1
rjd <3246251196ryan@gmail.com>
2023-06-06 09:21:17 +0100

    Added a few other methods in order to test out potential libstdc++ library multi definition conflicts with c library

--- tests/9_rjd_math_linking/main.cpp
+++ tests/9_rjd_math_linking/main.cpp
@@ -5,7 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-  float res = sinf(PI/2.0F);
-  printf("%.3f\n", res);
+  (void)argc; (void) argv;
+  { /* Float Math: SIN */
+    float res = sinf(PI/2.0F);
+    printf("%.3f\n", res);
+  } /**/
+
+  { /* Long Math: FLOOR */
+    long double res = floorl( 42.49L);
+    printf("%.3Lf\n", res);
+  } /**/
+
+  { /* "Double": CEIL */
+    double res = ceil( 42.49 /* defaults to double */);
+    printf("%.3f\n", res);
+  }
   return 0;
 }
