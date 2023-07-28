9147fb3217b5d8cedc0a5c9fbc2203d3116e94f3
rjd <3246251196ryan@gmail.com>
2023-07-28 18:41:58 +0100

    added signbit test

---
+++ tests/11_signbit/main.cpp
@@ -0,0 +1,14 @@
+/* RJD: Test from: https://cplusplus.com/reference/cmath/signbit/ */
+
+/* signbit example */
+#include <stdio.h>      /* printf */
+#include <math.h>       /* signbit, sqrt */
+
+int main()
+{
+  printf ("signbit(0.0)       : %d\n",signbit(0.0));
+  printf ("signbit(1.0/0.0)   : %d\n",signbit(1.0/0.0));
+  printf ("signbit(-1.0/0.0)  : %d\n",signbit(-1.0/0.0));
+  printf ("signbit(sqrt(-1.0)): %d\n",signbit(sqrt(-1.0)));
+  return 0;
+}
