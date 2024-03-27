65868a703ae1ee3020c8cce4e68905d4d5aa5dce
rjd <3246251196ryan@gmail.com>
2024-03-27 00:16:34 +0000

    updated test 20 to look for C++ mangled symbol

--- tests/20_rjd_runtimeLoadSO_dynld_cpp/main.cpp
+++ tests/20_rjd_runtimeLoadSO_dynld_cpp/main.cpp
@@ -2,22 +2,33 @@
 #include <iostream>
 
 std::string so_str = "librelo.so";
-std::string sym_str = "add42";
+std::string sym_str = "_Z5add42i"; /* c++ mangled */
+std::string sym_str_c = "add42another";
 
 int main( int argc, char *argv[] )
 {
   (void)argc;
   (void)argv;
   int ret = 10;
-  void *dl = dlopen( so_str.c_str(), RTLD_LAZY ), *sym = NULL;
+  void *dl = dlopen( so_str.c_str(), RTLD_LAZY ), *sym = NULL, *sym2 = NULL;
   if( dl )
     {
       sym = dlsym( dl, sym_str.c_str() );
       if ( sym )
 	{
 	  int ans = ((int (*)(int))(sym))( 42 );
-	  std::cout << "Answer " << ans << std::endl;
-	  ret = 0;
+
+	  sym2 = dlsym( dl, sym_str_c.c_str() );
+	  if ( sym2 )
+	    {
+	      int ans2 = ((int (*)(int))(sym2))( ans );
+	      std::cout << "Answer: " << ans2 << std::endl;
+	      ret = 0;
+	    }
+	  else
+	    {
+	      std::cerr << "Unable to locate symbol: `" << sym_str_c << "'" << std::endl;
+	    }
 	}
       else
 	{
--- tests/20_rjd_runtimeLoadSO_dynld_cpp/relo.cpp
+++ tests/20_rjd_runtimeLoadSO_dynld_cpp/relo.cpp
@@ -2,3 +2,11 @@ int add42( int x )
 {
   return x+42;
 }
+
+extern "C"
+{
+  int add42another( int x )
+  {
+    return x+42;
+  }
+}
