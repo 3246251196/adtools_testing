3887d1aa68239c74d5b15e522b729ccb3c4b9379
rjd <3246251196ryan@gmail.com>
2024-12-30 22:43:22 +0000

    Move printing to stdout

--- tests/22_ThreadLocalStorage/tls.c
+++ tests/22_ThreadLocalStorage/tls.c
@@ -5,16 +5,16 @@ __thread int i;
 
 void *fn( void* )
 {
-  fprintf(stderr, "Created thread addr of i: %p\n",&i);
+  printf("Created thread addr of i: %p\n",&i);
   i = 0;
   while(i++ < 1000000000);
-  fprintf(stderr, "Created thread val of i : %i\n",i);
+  printf("Created thread val of i : %i\n",i);
 }
 
 int main(void)
 {
   pthread_t t;
-  fprintf(stderr, "Parent thread addr of i : %p\n",&i);
+  printf("Parent thread addr of i : %p\n",&i);
   if ( pthread_create( &t, NULL, fn, NULL ) )
     {
       return 10;
@@ -25,6 +25,6 @@ int main(void)
       return 10;
     }
 
-  fprintf(stderr, "Parent thread val of i  : %i\n",i);
+  printf("Parent thread val of i  : %i\n",i);
   return 0;
 }
