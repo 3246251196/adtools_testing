592c838b0a3e9ed46b8fd79cfa51b55b6ecadce3
rjd <3246251196ryan@gmail.com>
2025-01-01 03:52:06 +0000

    Remove STDOUT with addr

--- tests/22_ThreadLocalStorage/tls.c
+++ tests/22_ThreadLocalStorage/tls.c
@@ -13,7 +13,6 @@ THR int i;
 
 void *fn( void* )
 {
-  printf("Created thread addr of i: %p\n",&i);
   i = 0;
   while(i++ < SIZE);
   printf("Created thread val of i : %i\n",i);
@@ -22,7 +21,6 @@ void *fn( void* )
 int main(void)
 {
   pthread_t t;
-  printf("Parent thread addr of i : %p\n",&i);
   if ( pthread_create( &t, NULL, fn, NULL ) )
     {
       return 10;
