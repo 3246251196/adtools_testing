b7b2ebc662e0629f4598ed96cf160236f6a2214b
rjd <3246251196ryan@gmail.com>
2025-03-14 23:10:10 +0000

    Do not use plugins in the test code. The function should already be resolved

--- tests/25_rpathUnixStyle_linktimeSo/main.c
+++ tests/25_rpathUnixStyle_linktimeSo/main.c
@@ -1,37 +1,11 @@
-#include <dlfcn.h>
 #include <stdio.h>
 
-const char so_str[] = "librelo.so", sym_str[] = "add42";
+extern int add42(int);
 
-int main( int argc, char *argv[] )
+int main( void )
 {
-  (void)argc;
-  (void)argv;
-  int ret = 10;
-  void *dl = dlopen( so_str, RTLD_LAZY ), *sym = NULL;
-  if( dl )
-    {
-      sym = dlsym( dl, sym_str );
-      if ( sym )
-	{
-	  int ans = ((int (*)(int))(sym))( 42 );
-	  printf("Answer: %d\n", ans);
-	  ret = 0;
-	}
-      else
-	{
-	  fprintf( stderr, "Unable to locate symbol: `%s'\n", sym_str );
-	}
-    }
-  else
-    {
-      fprintf( stderr, "Unable to load shared object: `%s'\n", so_str );
-    }
+  int ans = add42( 42 );
+  printf("Answer: %d\n", ans);
 
-  if ( dl )
-    {
-      dlclose( dl );
-    }
-
-  return ret;
+  return 0;
 }
