793ee02580d2a2c5fd50761dc391b168b390caf7
rjd <3246251196ryan@gmail.com>
2025-03-15 23:15:38 +0000

    Added a simple SO test

---
+++ tests/26_simpleSOTest/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sharedObj.h"
+
+int main( void )
+{
+	int ret = fn();
+	return ret;
+}
---
+++ tests/26_simpleSOTest/sharedObj.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sharedObj.h"
+
+int fn( void )
+{
+	printf( "Inside Shared Object function: fn()\n");
+	return 0;
+}
